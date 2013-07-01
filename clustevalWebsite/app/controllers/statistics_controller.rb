3c4b9d51493c40a8297e5d28e11080117c11d026
wiwie <derwiwie@googlemail.com>
2013-07-01 15:24:39 +0200

    bugfix: renamed tables statistics_datas->statistics_data and statistics_run_datas->statistics_run_data; added controller methods and views for showing and listing statistics

--- clustevalWebsite/app/controllers/statistics_controller.rb
+++ clustevalWebsite/app/controllers/statistics_controller.rb
@@ -1,3 +1,13 @@
 class StatisticsController < ApplicationController
 	before_filter :require_user
+
+	def show
+
+	end
+
+	def index
+		@dataStatistics = StatisticsData.all(session).sort_by{|x| x.statistic.alias}
+		@runStatistics = StatisticsRun.all(session).sort_by{|x| x.statistic.alias}
+		@runDataStatistics = StatisticsRunData.all(session).sort_by{|x| x.statistic.alias}
+	end
 end
