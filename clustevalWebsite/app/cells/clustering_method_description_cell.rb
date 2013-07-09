d8b629ab4fab2838e27263cab80633468c8785f2
wiwie <derwiwie@googlemail.com>
2013-07-09 12:46:57 +0200

    removed unnecessary program description files

--- clustevalWebsite/app/cells/clustering_method_description_cell.rb
+++ clustevalWebsite/app/cells/clustering_method_description_cell.rb
@@ -3,7 +3,6 @@ class ClusteringMethodDescriptionCell < Cell::Rails
 
 	def display(opts)
 		begin
-	  		#render :view => 'display_' + opts[:method].full_name.gsub('/','_'), :locals => {:method => opts[:method]}
 	  		render :view => 'display', :locals => {:method => opts[:method], :detailsLink => opts[:detailsLink]}
 	  	rescue
 	  		render :text => opts[:method].full_name + '<br />'
