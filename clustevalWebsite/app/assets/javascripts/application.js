67b0952c23d02b2de214d7fdfcb1b92fce56baa8
wiwie <derwiwie@googlemail.com>
2013-07-16 14:07:45 +0200

    started cleaning up structure of website: renaming

--- clustevalWebsite/app/assets/javascripts/application.js
+++ clustevalWebsite/app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require bootstrap/bootstrap-tooltip
 //= require dataTables/jquery.dataTables
 //= require dataTables/TableTools.min
 //= require_tree .
