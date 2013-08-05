3b56a2f963fd87c6a3fe1c548d67de26c223be2b
wiwie <derwiwie@googlemail.com>
2013-08-05 20:54:59 +0200

    updated aboutus and made some other minor changes

---
+++ clustevalWebsite/config/sitemap.rb
@@ -0,0 +1,35 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://clusteval.mpi-inf.mpg.de"
+
+SitemapGenerator::Sitemap.create do
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+  add '/mains/comparison'
+  add programs_path
+  add datasets_path
+  add clustering_quality_measures_path
+  add submits_path
+  add admins_path
+  add helps_path
+  add aboutus_path
+end
