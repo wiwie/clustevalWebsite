879165bcd677c1eabceb6ef767ad0b5d84f4491b
wiwie <derwiwie@googlemail.com>
2013-11-30 17:17:21 +0100

    reworked workflow -> is now an svg graphics instead of javascript

--- clustevalWebsite/app/controllers/data_configs_controller.rb
+++ clustevalWebsite/app/controllers/data_configs_controller.rb
@@ -2,9 +2,7 @@ class DataConfigsController < ApplicationController
 	before_filter :require_user
 
 	def index
-		@repositoryType = RepositoryType.find_by_name("Repository")
-		@repository = Repository.find_by_repository_type_id(@repositoryType.id)
-		@dataConfigs = DataConfig.where(:repository_id => @repository.id)
+		@dataConfigs = DataConfig.all(session)
 		
 		respond_to do |format|
 			format.html # index.html.erb
@@ -13,7 +11,7 @@ class DataConfigsController < ApplicationController
 	end
 
 	def fetch_table_data
-		@dataConfig = DataConfig.find_by_name(params[:id])
+		@dataConfig = DataConfig.all(session).select{|dataConfig| dataConfig.name == params[:id]}.first
 		@datasetConfig = @dataConfig.dataset_config
 		@goldstandardConfig = @dataConfig.goldstandard_config
     	@runResults = RunResultsParameterOptimization.select(:id).where(:data_config_id => DataConfig.select(:id).where(:data_config_id => params[:id]))
@@ -27,8 +25,9 @@ class DataConfigsController < ApplicationController
 			@paramValuesQualityArray << [
 				runResult.t12_r5, 
 				runResult.t10_r6.split('/')[-1], 
+				#runResult.t10_r6,
 				runResult.t0_r3.gsub(',','<br />'),
-				#(@measure.nil? ? '' : @measure.alias),
+				#runResult.t0_r3,
 				runResult.t5_r2,
         		runResult.t4_r4, 
 				#''#view_context.link_to("Clustering", :controller => "run_results_parameter_optimizations_parameter_set_iterations", :action=>"show", :id => runResult.t1_r2).to_s
@@ -37,12 +36,12 @@ class DataConfigsController < ApplicationController
 			]
 		end
 
-		@json = {"aaData" => @paramValuesQualityArray}.to_json
+		@json = JSON.generate({"aaData" => @paramValuesQualityArray})
 		render :inline => @json
 	end
 	
 	def show
-		@dataConfig = DataConfig.find_by_name(params[:id])
+		@dataConfig = DataConfig.all(session).select{|dataConfig| dataConfig.name == params[:id]}.first
 		@datasetConfig = @dataConfig.dataset_config
 		@goldstandardConfig = @dataConfig.goldstandard_config
     	@runResults = RunResultsParameterOptimization.select(:id).where(:data_config_id => DataConfig.select(:id).where(:data_config_id => params[:id]))
--- clustevalWebsite/app/controllers/mains_controller.rb
+++ clustevalWebsite/app/controllers/mains_controller.rb
@@ -23,7 +23,6 @@ class MainsController < ApplicationController
 	end
 
 	def comparison
-
 		if params[:post]
 			@qualityMeasure = ClusteringQualityMeasure.find_by_id(params[:post][:measure])
 			@inverted = params[:inv]
@@ -49,6 +48,155 @@ class MainsController < ApplicationController
 			format.html # index.html.erb
 			format.json { render :json => json }
 		end
+	end
+
+	def workflow
+	end
+
+	def comparison_table_data
+		# TODO: inverse
+		@matrix = []
+		@datasetIds = {}
+		@datasets = Dataset.all(session)
+		for i in 0..@datasets.length-1
+		  @datasetIds[@datasets[i]] = i
+		  @matrix << ['<a href="' + url_for({:only_path => true, :controller => 'datasets', :action => 'show', :id => @datasets[i]}) + '"/>' + @datasets[i].alias + '</a>']
+		end
+		@programIds = {}
+		@programs = Program.all(session).sort_by{|x| x.alias}
+		for i in 1..@programs.length
+		  @programIds[@programs[i-1]] = i
+
+		  for j in 0..@datasets.length-1
+		    @matrix[j][i] = '--'
+		  end
+		end
+		@qualityMeasure = ClusteringQualityMeasure.find_by_id(params[:qualityMeasure])
+		@isMaximum = @qualityMeasure.optimum == 'Maximum'
+		@rowMax = {}
+		@rowMaxPos = {}
+		@iterationsExts = ParameterOptimizationIterationsExt.includes(:program, :dataset).select("program_id,dataset_id,max(quality) as maxQuality,min(quality) as minQuality,clustering_quality_measure_id").where(:clustering_quality_measure_id => @qualityMeasure).group("dataset_id,program_id")
+		@iterationsExts.each do |iterationExt|
+		  datasetId = @datasetIds[iterationExt.dataset]
+		  programId = @programIds[iterationExt.program]
+
+		  next if datasetId == nil or programId == nil
+
+		  @matrix[datasetId][programId] = (((@isMaximum ? iterationExt.maxQuality : iterationExt.minQuality)*1000).round/1000.0)
+		  if @rowMax[datasetId]
+		  	if @isMaximum
+			    if @rowMax[datasetId] < @matrix[datasetId][programId]
+			      @rowMax[datasetId] = @matrix[datasetId][programId]
+			      @rowMaxPos[datasetId] = [programId]
+			    elsif @rowMax[datasetId] == @matrix[datasetId][programId]
+			      @rowMaxPos[datasetId] << programId
+			    end
+			else
+			    if @rowMax[datasetId] > @matrix[datasetId][programId]
+			      @rowMax[datasetId] = @matrix[datasetId][programId]
+			      @rowMaxPos[datasetId] = [programId]
+			    elsif @rowMax[datasetId] == @matrix[datasetId][programId]
+			      @rowMaxPos[datasetId] << programId
+			    end
+			end
+		  else
+		    @rowMax[datasetId] = @matrix[datasetId][programId]
+		    @rowMaxPos[datasetId] = [programId]
+		  end
+		end
+
+		for i in 0..@datasets.length-1
+			for j in 1..@programs.length
+			    if not @matrix[i][j].to_s == "--"
+			        @string = '<a href="/datasets/' + @datasets[i-1].id.to_s + '/bestclust/' + @programs[j-1].alias + '?post[clustering_quality_measure_id2]=' + @qualityMeasure.id.to_s + '">'
+
+			        if @rowMaxPos[i] && @rowMaxPos[i].include?(j)
+			          @string = @string + "<b>"
+			        end
+
+			        @string = @string + @matrix[i][j].to_s + '</a>'
+
+			        if @rowMaxPos[i] && @rowMaxPos[i].include?(j)
+			          @string = @string + "</b>"
+			        end
+			        @matrix[i][j] = @string
+			    end
+			end
+		end
+		render :inline => {"aaData" => @matrix}.to_json
+	end
+
+	def comparison_table_data_inverse
+		@matrix = []
+		@programIds = {}
+		@datasets = Dataset.all(session).sort_by{|x| x.name}
+		@programs = Program.all(session)
+		for i in 0..@programs.length-1
+		  @programIds[@programs[i]] = i
+		  @matrix << ['<a href="' + url_for({:only_path => true, :controller => 'programs', :action => 'show', :id => @programs[i]}) + '"/>' + @programs[i].alias + '</a>']
+		end
+		@datasetIds = {}
+		for i in 1..@datasets.length
+		  @datasetIds[@datasets[i-1]] = i
+
+		  for j in 0..@programs.length-1
+		    @matrix[j][i] = '--'
+		  end
+		end
+		@qualityMeasure = ClusteringQualityMeasure.find_by_id(params[:qualityMeasure])
+		@isMaximum = @qualityMeasure.optimum == 'Maximum'
+
+		@rowMax = {}
+		@rowMaxPos = {}
+		@iterationsExts = ParameterOptimizationIterationsExt.includes(:program, :dataset).select("program_id,dataset_id,max(quality) as maxQuality,min(quality) as minQuality,clustering_quality_measure_id").where(:clustering_quality_measure_id => @qualityMeasure).group("dataset_id,program_id")
+		@iterationsExts.each do |iterationExt|
+		  datasetId = @datasetIds[iterationExt.dataset]
+		  programId = @programIds[iterationExt.program]
+
+		  next if datasetId == nil or programId == nil
+
+		  @matrix[programId][datasetId] = (((@isMaximum ? iterationExt.maxQuality : iterationExt.minQuality)*1000).round/1000.0)
+		  if @rowMax[programId]
+		  	if @isMaximum
+			    if @rowMax[programId] < @matrix[programId][datasetId]
+			      @rowMax[programId] = @matrix[programId][datasetId]
+			      @rowMaxPos[programId] = [datasetId]
+			    elsif @rowMax[programId] == @matrix[programId][datasetId]
+			      @rowMaxPos[programId] << datasetId
+			    end
+			else
+			    if @rowMax[programId] > @matrix[programId][datasetId]
+			      @rowMax[programId] = @matrix[programId][datasetId]
+			      @rowMaxPos[programId] = [datasetId]
+			    elsif @rowMax[programId] == @matrix[programId][datasetId]
+			      @rowMaxPos[programId] << datasetId
+			    end
+			end
+		  else
+		    @rowMax[programId] = @matrix[programId][datasetId]
+		    @rowMaxPos[programId] = [datasetId]
+		  end
+		end
+
+		for i in 0..@programs.length-1
+			for j in 1..@datasets.length
+			    if not @matrix[i][j].to_s == "--"
+			        @string = '<a href="/datasets/' + @datasets[j-1].id.to_s + '/bestclust/' + @programs[i-1].alias + '?post[clustering_quality_measure_id2]=' + @qualityMeasure.id.to_s + '">'
+
+			        if @rowMaxPos[i] && @rowMaxPos[i].include?(j)
+			          @string = @string + "<b>"
+			        end
+
+			        @string = @string + @matrix[i][j].to_s + '</a>'
+
+			        if @rowMaxPos[i] && @rowMaxPos[i].include?(j)
+			          @string = @string + "</b>"
+			        end
+			        @matrix[i][j] = @string
+			    end
+			end
+		end
+		render :inline => {"aaData" => @matrix}.to_json
 	end
 
 	def set_user_session_repository
