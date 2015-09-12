0b324871e0f43c8b0c3d7ae015c88ab4b013fd3e
wiwiec <derwiwie@googlemail.com>
2015-09-12 10:34:58 +0200

    using newest twitter bootstrap version

--- clustevalWebsite/config/admin_navigation.rb
+++ clustevalWebsite/config/admin_navigation.rb
@@ -316,7 +316,7 @@ SimpleNavigation::Configuration.run do |navigation|
     # you can also specify a css id or class to attach to this particular level
     # works for all levels of the menu
     # primary.dom_id = 'menu-id'
-    primary.dom_class = 'nav'
+    primary.dom_class = 'nav nav-tabs'
 
     # You can turn off auto highlighting for a specific level
     # primary.auto_highlight = false
