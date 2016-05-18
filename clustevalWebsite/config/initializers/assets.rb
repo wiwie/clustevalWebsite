6369cb2415f962037fc7813b3d1e84d0d9b900a9
wiwiec <derwiwie@googlemail.com>
2016-05-19 00:44:46 +0200

    added some initializer statements for assets

---
+++ clustevalWebsite/config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( stylesheet.css )
+Rails.application.config.assets.precompile += %w( raphael.js )
+Rails.application.config.assets.precompile += %w( ColReorderWithResize.js )
